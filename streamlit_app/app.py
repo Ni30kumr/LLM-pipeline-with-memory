3a2ae14c7c9193636eff653d66520422f411fe56
Nitesh Kumar <niteshkumaar87@gmail.com>
2025-05-11 01:21:26 +0530

    updated .env

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,33 +1,3 @@
-# import streamlit as st
-# import requests
-
-# st.title("LLM-based RAG Search")
-
-# # Input for user query
-# query = st.text_input("Enter your query:")
-
-# if st.button("Search"):
-#     st.write("accessing Flask app with query:", query)
-
-#     try:
-#         # ✅ Make the POST request to Flask API
-#         response = requests.post(
-#             "http://localhost:5001/query",
-#             json={"query": query},
-#             timeout=15
-#         )
-
-#         if response.status_code == 200:
-#             answer = response.json().get('answer', "No answer received.")
-#             st.success("Answer:")
-#             st.write(answer)
-#         else:
-#             st.error(f"Error: {response.status_code} - {response.text}")
-
-#     except requests.exceptions.RequestException as e:
-#         st.error(f"Failed to connect to the Flask server: {e}")
-
-
 import streamlit as st
 import requests
 
